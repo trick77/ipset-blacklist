d8384deb404e9460f36ab9b3179208dcd3f52b62
Jan <jan@trick77.com>
2013-10-12 21:14:35 +0200

    first commit

---
+++ update-blacklist.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+IP_TMP=/tmp/ip.tmp
+IP_BLACKLIST=/etc/ip-blacklist.conf
+IP_BLACKLIST_TMP=/tmp/ip-blacklist.tmp
+BLACKLISTS=(
+"http://www.projecthoneypot.org/list_of_ips.php?t=d&rss=1" # Project Honey Pot Directory of Dictionary Attacker IPs
+"http://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=1.1.1.1"  # TOR Exit Nodes
+"http://www.maxmind.com/en/anonymous_proxies" # MaxMind GeoIP Anonymous Proxies
+"http://danger.rulez.sk/projects/bruteforceblocker/blist.php" # BruteForceBlocker IP List
+"http://rules.emergingthreats.net/blockrules/rbn-ips.txt" # Emerging Threats - Russian Business Networks List
+"http://www.spamhaus.org/drop/drop.lasso" # Spamhaus Don't Route Or Peer List (DROP)
+"http://cinsscore.com/list/ci-badguys.txt" # C.I. Army Malicious IP List
+"http://www.openbl.org/lists/base.txt"  # OpenBL.org 30 day List
+"http://www.autoshun.org/files/shunlist.csv" # Autoshun Shun List
+)
+for i in "${BLACKLISTS[@]}"
+do
+    curl "$i" > $IP_TMP
+    grep -Po '(?:\d{1,3}\.){3}\d{1,3}(?:/\d{1,2})?' $IP_TMP >> $IP_BLACKLIST_TMP
+done
+sort $IP_BLACKLIST_TMP -n | uniq > $IP_BLACKLIST
+rm $IP_BLACKLIST_TMP
+wc -l $IP_BLACKLIST
+
