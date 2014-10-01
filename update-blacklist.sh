72837578fa2dd004e9566fcc7ce4c10c616fdbad
br1cked <br1cked@users.noreply.github.com>
2014-10-01 12:17:50 -1000

    busybox (ash) compatibility & memory optimizations
    changed bash syntax to ash syntax & used pipes instead of temporary files in /tmp to save memory

--- update-blacklist.sh
+++ update-blacklist.sh
@@ -1,37 +1,52 @@
-#!/bin/bash
+#!/bin/sh
+
+# config
 IP_BLACKLIST=/etc/ip-blacklist.conf
-IP_BLACKLIST_TMP=/tmp/ip-blacklist.tmp
 WGET_LOG=/tmp/update_blacklist.log
-BLACKLISTS=(
-"http://www.projecthoneypot.org/list_of_ips.php?t=d&rss=1" # Project Honey Pot Directory of Dictionary Attacker IPs
-"http://check.torproject.org/exit-addresses"  # TOR Exit Nodes
-"http://www.maxmind.com/en/anonymous_proxies" # MaxMind GeoIP Anonymous Proxies
-"http://www.spamhaus.org/drop/drop.lasso" # Spamhaus Don't Route Or Peer List (DROP)
-"http://danger.rulez.sk/projects/bruteforceblocker/blist.php" # BruteForceBlocker IP List
-"http://www.openbl.org/lists/base.txt" # OpenBL.org 30 day List
-"http://cinsscore.com/list/ci-badguys.txt" # C.I. Army Malicious IP List
-"http://www.autoshun.org/files/shunlist.csv" # Autoshun Shun List
-"http://lists.blocklist.de/lists/all.txt" # blocklist.de attackers
-"http://rules.emergingthreats.net/blockrules/emerging-compromised.rules" # Emerging Threats - Compromised Hosts
-)
+BLACKLISTS="http://www.projecthoneypot.org/list_of_ips.php?t=d&rss=1
+http://check.torproject.org/exit-addresses
+http://www.maxmind.com/en/anonymous_proxies
+http://www.spamhaus.org/drop/drop.lasso
+http://danger.rulez.sk/projects/bruteforceblocker/blist.php
+http://www.openbl.org/lists/base.txt
+http://cinsscore.com/list/ci-badguys.txt
+http://www.autoshun.org/files/shunlist.csv
+http://lists.blocklist.de/lists/all.txt
+http://rules.emergingthreats.net/blockrules/emerging-compromised.rules"
 
-rm $WGET_LOG
+# download multiple URLs and output to stdout (log errors to logfile)
+download_lists(){
+    
+    # clear the wget log
+    echo -n > $WGET_LOG
 
-for i in "${BLACKLISTS[@]}"
-do
-    wget -O - "$i" 2>> $WGET_LOG | sed -r 's/,/\n/g' | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}" >> $IP_BLACKLIST_TMP
+    # download blacklists
+    for i in $*
+    do
+        wget -O - "$i" 2>> $WGET_LOG | sed -r 's/,/\n/g' | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}"
+    done
+}
+
+# create the temporary ipset, exit on error
+ipset create blacklist_tmp hash:net || exit 1
+
+# download the blacklists
+download_lists $BLACKLISTS |\
+
+# remove duplicate IPs
+sort -u |\
+
+# save blacklist to disk so it can be restored at boot
+tee $IP_BLACKLIST |\
+
+# iterate through list, saving IPs in temporary ipset
+egrep -v "^#|^$" | while IFS= read -r ip
+do 
+    ipset add blacklist_tmp $ip
 done
 
-sort -u $IP_BLACKLIST_TMP > $IP_BLACKLIST
-rm $IP_BLACKLIST_TMP
-wc -l $IP_BLACKLIST
-
-ipset create blacklist_tmp hash:net
-
-egrep -v "^#|^$" $IP_BLACKLIST | while IFS= read -r ip
-do
-ipset add blacklist_tmp $ip
-done
-
+# activate the new blacklist
 ipset swap blacklist blacklist_tmp
+
+# destroy the temporary blacklist (now contains the old blacklist)
 ipset destroy blacklist_tmp
