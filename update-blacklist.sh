16c7a7c32652f891e1cddd74905ec77ca4114d2d
br1cked <br1cked@users.noreply.github.com>
2014-10-08 00:39:53 -1000

    added ipset maxelem option & more blacklist URLs

--- update-blacklist.sh
+++ update-blacklist.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 IP_BLACKLIST=/etc/ip-blacklist.conf
+TEMP_FILE_NAME=/tmp/blacklist_temp
 WGET_LOG=/tmp/update_blacklist.log
 BLACKLISTS="
     http://www.projecthoneypot.org/list_of_ips.php?t=d&rss=1
@@ -12,7 +13,10 @@ BLACKLISTS="
     http://cinsscore.com/list/ci-badguys.txt
     http://www.autoshun.org/files/shunlist.csv
     http://lists.blocklist.de/lists/all.txt
-    http://rules.emergingthreats.net/blockrules/emerging-compromised.rules"
+    http://rules.emergingthreats.net/blockrules/emerging-compromised.rules
+    http://malc0de.com/bl/IP_Blacklist.ttx
+    http://www.dshield.org/ipsascii.html?limit=10000
+    "
 
 # download multiple URLs and output to stdout (log errors to logfile)
 download_lists(){
@@ -38,8 +42,8 @@ parse_list(){
     sort -u
 }
 
-# create the temporary ipset, exit on error
-ipset create blacklist_tmp hash:net || exit 1
+# rename temp file variable name
+$NUM_IPS=$TEMP_FILE_NAME
 
 # download the blacklists
 download_lists $BLACKLISTS |\
@@ -47,11 +51,22 @@ download_lists $BLACKLISTS |\
 # parse the blacklists
 parse_list |\
 
+# limit the number of IPs to 0xffffffff (ipset hardcoded maximum) (NOTE: this is also the number of possible IPv4 addresses)
+#head -n 4294967295 |\
+
 # save blacklist to disk so it can be restored at boot
 tee $IP_BLACKLIST |\
 
+# count number of IPs
+wc -l > $NUM_IPS
+
+# create the temporary ipset
+ipset create blacklist_tmp hash:net maxelem `cat $NUM_IPS`
+
+rm $NUM_IPS
+
 # save each IP in the temporary ipset
-while IFS= read -r ip
+< $IP_BLACKLIST while IFS= read -r ip
 do 
     ipset add blacklist_tmp $ip
 done
