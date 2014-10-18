3daf634e16c6740fa2b54f53f108b3a1d05bea92
br1ckd <br1ckd@null.net>
2014-10-17 15:25:54 -1000

    added checks for failed commands

--- update-blacklist.sh
+++ update-blacklist.sh
@@ -34,7 +34,7 @@ download_lists(){
     # download blacklists
     for i in $*
     do
-        wget --no-check-certificate -O - "$i" 2>> $WGET_LOG
+        if wget --no-check-certificate -O - "$i" 2>> $WGET_LOG; then echo Error downloading $i && return 1; fi
     done
 }
 
@@ -44,7 +44,7 @@ process_raw_list(){
     sed -r 's/,/\n/g' |\
     
     # drop the lines without IP addresses
-    grep -E $IP_REGEX
+    grep -E $IP_REGEX || return 1
 }
 
 # take a list of IPs or nets on stdin (1 per line) and create & apply an ipset
@@ -55,13 +55,13 @@ create_ipset(){
     NUM_ELEMS=`tee $TEMP_DIR/temp_list | wc -l`
 
     # create the temporary ipset
-    ipset create $2_tmp $1 maxelem $NUM_ELEMS
+    if ! ipset create $2_tmp $1 maxelem $NUM_ELEMS; then return 1; fi
     
     # fill the ipset
     cat $TEMP_DIR/temp_list | while IFS= read -r elem
     do
         #echo "adding $elem to $2"
-        ipset add $2_tmp $elem
+        if ! ipset add $2_tmp $elem ; then return 1; fi
     done
     
     # try to swap the temp ipset with the final ipset. 
@@ -72,34 +72,37 @@ create_ipset(){
         then
             echo Error renaming ipset >&2
             ipset destroy $2_tmp
-            return -1
+            return 1
         fi
     fi
 
     # destroy the old ipset
-    ipset destroy $2_tmp
+    if ! ipset destroy $2_tmp; then return 1; fi
+
+    echo Created ipset $2 with $NUM_ELEMS elements
 }
 
 # make the temp dir
-mkdir -p $TEMP_DIR
+if ! mkdir -p $TEMP_DIR; then echo Error making temp dir && exit; fi
 
 # download the raw list of suspicious hosts & nets
-download_lists $BLACKLISTS | process_raw_list > $TEMP_DIR/raw
+if ! (download_lists $BLACKLISTS | process_raw_list > $TEMP_DIR/raw); then echo Error downloading blacklists && exit; fi
 
 # create blacklist for IP nets
-(grep -oE "$CIDR_REGEX" $TEMP_DIR/raw | uniq_cidr.lua &
+if ! ((grep -oE "$CIDR_REGEX" $TEMP_DIR/raw | uniq_cidr.lua &
 grep -oE "$RANGE_REGEX" $TEMP_DIR/raw & wait) |\
 tee $NET_BLACKLIST |\
-create_ipset hash:net blacklist_net
+create_ipset hash:net blacklist_net); then echo Error creating hash:net blacklist && exit; fi
 
 
 # create blacklist for individual IP addrs
-grep -vE "$NET_REGEX" $TEMP_DIR/raw |\
+if ! (grep -vE "$NET_REGEX" $TEMP_DIR/raw |\
 grep -oE "$IP_REGEX" |\
 sort -u |\
 tee $IP_BLACKLIST |\
-create_ipset hash:ip blacklist_ip
+create_ipset hash:ip blacklist_ip); then echo Error creating hash:ip blacklist && exit; fi
 
 
 # delete temp dir
 rm -rf $TEMP_DIR
+
