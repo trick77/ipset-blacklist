cc9d5317d081db97d16d9e052c84f3fffb3173a6
br1ckd <br1ckd@null.net>
2014-11-12 07:04:09 -1000

    updated install script

--- install.sh
+++ install.sh
@@ -3,18 +3,20 @@
 
 opkg install ipset wget || (opkg update && opkg install ipset wget) || exit
 
-
+# copy the scripts to the PATH
 mkdir -p /usr/local/bin
+if ! grep /usr/local/bin /etc/profile; then echo "PATH=$PATH:/usr/local/bin" >> /etc/profile; fi
 cp update_blacklist.sh load_blacklist.sh normalize_ip.lua uniq_cidr.lua /usr/local/bin
 cp bindechex.lua /usr/share/lua
 
-
+# load blacklists with firewall rules at boot
 if ! grep load_blacklist.sh /etc/firewall.user
 then
 	echo "/bin/nice /bin/ionice /usr/local/bin/load_blacklist.sh ip /etc/blacklist.ip.conf" >> /etc/firewall.user
 	echo "/bin/nice /bin/ionice /usr/local/bin/load_blacklist.sh net /etc/blacklist.net.conf" >> /etc/firewall.user
 fi
 
+# add cron job to update the blacklists (don't set this to update too often or you might get banned)
 if ! grep update_blacklist.sh /etc/crontabs/root
 then
 	echo "17 10 * * * sleep 5 && nice ionice update-blacklist.sh > /tmp/update_blacklist.log 2>&1" >> /etc/crontabs/root
