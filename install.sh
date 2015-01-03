0d49824e352113f5df1324856bf321c7a5645cb8
br1cked <br1cked@users.noreply.github.com>
2015-01-03 13:33:26 -1000

    added lua to required packages, and removed ionice

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 # Install ipset blacklist scripts
 
-opkg install ipset wget || (opkg update && opkg install ipset wget) || exit
+REQUIRED_PACKAGES="ipset wget lua"
+
+opkg install $REQUIRED_PACKAGES || (opkg update && opkg install $REQUIRED_PACKAGES) || exit
 
 # copy the scripts to the PATH
 mkdir -p /usr/local/bin
@@ -12,12 +14,12 @@ cp bindechex.lua /usr/share/lua
 # load blacklists with firewall rules at boot
 if ! grep load_blacklist.sh /etc/firewall.user
 then
-	echo "/bin/nice /bin/ionice /usr/local/bin/load_blacklist.sh ip /etc/blacklist.ip.conf" >> /etc/firewall.user
-	echo "/bin/nice /bin/ionice /usr/local/bin/load_blacklist.sh net /etc/blacklist.net.conf" >> /etc/firewall.user
+	echo "/bin/nice /usr/local/bin/load_blacklist.sh ip /etc/blacklist.ip.conf" >> /etc/firewall.user
+	echo "/bin/nice /usr/local/bin/load_blacklist.sh net /etc/blacklist.net.conf" >> /etc/firewall.user
 fi
 
 # add cron job to update the blacklists (don't set this to update too often or you might get banned)
 if ! grep update_blacklist.sh /etc/crontabs/root
 then
-	echo "17 10 * * * sleep 5 && nice ionice update-blacklist.sh > /tmp/update_blacklist.log 2>&1" >> /etc/crontabs/root
+	echo "17 10 * * * sleep 5 && nice update-blacklist.sh > /tmp/update_blacklist.log 2>&1" >> /etc/crontabs/root
 fi
