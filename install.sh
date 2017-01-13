564bf5fc08022c8cb4f04a8dc1ff9058f723f809
Toshiya TSURU <turutosiya@gmail.com>
2017-01-13 15:41:10 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 \curl -sSL https://raw.githubusercontent.com/trick77/ipset-blacklist/master/update-blacklist.sh > /etc/cron.daily/update-blacklist.sh
+mkdir -p /etc/ipset-blacklist ; wget -O /etc/ipset-blacklist/ipset-blacklist.conf https://raw.githubusercontent.com/trick77/ipset-blacklist/master/ipset-blacklist.conf
 chmod +x /etc/cron.daily/update-blacklist.sh
 yum install -y ipset
 ipset create blacklist hash:net
-/etc/cron.daily/update-blacklist.sh
-iptables -I INPUT -m set --match-set blacklist src -j DROP
+/etc/cron.daily/update-blacklist.sh /etc/ipset-blacklist/ipset-blacklist.conf
+iptables -I INPUT 1 -m set --match-set blacklist src -j DROP
 iptables-save | tee /etc/sysconfig/iptables
